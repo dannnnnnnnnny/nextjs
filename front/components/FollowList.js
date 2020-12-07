495f58f8748348cbae84a12d76918f8b68f32091
dh_kim <ehdgnl5249@naver.com>
2020-12-07 14:42:34 +0900

    CSR & SSR 차이 검색엔진 해결 방법 수정

--- front/components/FollowList.js
+++ front/components/FollowList.js
@@ -1,20 +1,27 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { List, Button, Card } from 'antd';
 import { StopOutlined } from '@ant-design/icons';
 import PropTypes from 'prop-types';
 
 const FollowList = ({ header, data }) => {
+	const gridStyle = useMemo(() => ({ gutter: 4, xs: 2, md: 3}), []);
+	const ListStyle = useMemo(() => ({ marginBottom: 20 }), []);
+	const loadMoreStyle = useMemo(() => (
+		{ textAlign: 'center', margin: '10px 0' }
+	), []);
+	const ListItemStyle = useMemo(() => ({ margin: 20 }), []);
+
 	return (
 		<List 
-			style={{ marginBottom: 20 }}
-			grid={{ gutter: 4, xs: 2, md: 3 }}
+			style={ListStyle}
+			grid={gridStyle}
 			size="small"
 			header={<div>{header}</div>}
-			loadMore={<div style={{ textAlign: 'center', margin: '10px 0' }}><Button>더 보기</Button></div>}
+			loadMore={<div style={loadMoreStyle}><Button>더 보기</Button></div>}
 			bordered
 			dataSource={data}
 			renderItem={item => (
-				<List.Item style={{ margin: 20 }}>
+				<List.Item style={ListItemStyle}>
 					<Card
 						actions={[<StopOutlined key="stop" />]}
 					>
